fd0a5a381150d40b0a90222957fcf1b6caab9655
FelixAbrahamsson <felixab@kth.se>
2021-06-14 15:03:16 +0200

    feature: seekable stream

--- kw6/__init__.py
+++ kw6/__init__.py
@@ -1,7 +1,8 @@
+from kw6 import settings
 from kw6 import types
 from kw6.camera import Camera, CameraHeader
 from kw6.position import Position, PositionHeader
-from kw6.stream import Stream
+from kw6.stream import Stream, SeekableStream
 from kw6.to_png import to_png
 
 from pkg_resources import get_distribution, DistributionNotFound
--- kw6/stream.py
+++ kw6/stream.py
@@ -1,9 +1,8 @@
+import io
 from pathlib import Path
 from pydantic import BaseModel
 
-from kw6 import Position
-
-N_BYTES_DOUBLE = 8
+from kw6 import Position, PositionHeader, types
 
 
 class Stream(BaseModel):
@@ -54,6 +53,62 @@ class Stream(BaseModel):
                 yield Position.from_stream_(stream)
 
 
+class SeekableStream(BaseModel):
+    '''
+    Used to iterate over images in a kw6 file with seeking supported.
+
+    Example:
+
+    .. code-block:: python
+
+        from pathlib import Path
+        import kw6
+
+        path = Path('...')
+        stream = kw6.SeekableStream.from_path(path)
+        stream.seek_(25000)
+        for position in stream.read_positions_(200):
+            for camera in position.cameras:
+                camera.image.save(
+                    f'{position.header.frame_index}_{camera.header.camera_index}.png'
+                )
+    '''
+    stream: io.BufferedReader
+
+    class Config:
+        allow_mutation = False
+        arbitrary_types_allowed = True
+
+    @staticmethod
+    def from_path(path):
+        stream = Path(path).open('rb')
+        version = Stream.version_(stream)
+        if version != 'KW6FileClassVer1.0':
+            raise ValueError(f'Unexpected file version {version}')
+        return SeekableStream(stream=stream)
+
+    def seek_(self, frame_index: types.FRAME_INDEX):
+        '''Go to stream position indicated by frame_index'''
+        while (
+            PositionHeader.peek(self.stream).frame_index != frame_index
+            and self.stream.peek(1) != b''
+        ):
+            Position.skip_(self.stream)
+
+    def read_positions_(self, n: int):
+        '''Read n positions from current stream position'''
+        for i in range(n):
+            if self.stream.peek(1) == b'':
+                break
+            else:
+                yield Position.from_stream_(self.stream)
+
+    def __iter__(self):
+        '''Iterate over positions and cameras in the file from current stream position'''
+        while self.stream.peek(1) != b'':
+            yield Position.from_stream_(self.stream)
+
+
 def test_file_not_found():
     import pytest
 
@@ -70,3 +125,9 @@ def test_iter():
             max_position = position.header.frame_index
 
     assert max_position >= 50
+
+
+def test_seek():
+    stream = SeekableStream.from_path('test/test.kw6')
+    stream.seek_(10)
+    assert next(stream.read_positions_(1)).header.frame_index == 10
